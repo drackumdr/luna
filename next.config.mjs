302a260f240c454500b239f45c710dc0fb4d5850
Drackum <drackum@gmail.com>
2026-05-01 15:44:25 -0600

    push con pages  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   .github/workflows/deploy-github-pages.yml
    	renamed:    app/globals.css -> app-archive/globals.css
    	renamed:    app/layout.tsx -> app-archive/layout.tsx
    	renamed:    app/page.tsx -> app-archive/page.tsx
    	modified:   next.config.mjs
    	modified:   package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/luna',
+  assetPrefix: '/luna/',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
